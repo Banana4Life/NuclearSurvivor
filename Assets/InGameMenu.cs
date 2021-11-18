9d9fbdd1155fa174fd711237615c93b3398ae7d7
Anselm Brehme <development.anselm@breh.me>
2021-11-18 17:33:36 +0100

    some ui

--- Assets/InGameMenu.cs
+++ Assets/InGameMenu.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameMenu : MonoBehaviour
 {
     public RectTransform leftTime;
     public RectTransform rightTime;
 
+    public Text cubePoints; 
+    public Text barrelPoints; 
+
     public Game game;
     
-    private RectTransform thisRect;
+    private RectTransform parentTimeRect;
     void Start()
     {
-        thisRect = GetComponent<RectTransform>();
+        parentTimeRect = leftTime.parent.GetComponent<RectTransform>();
     }
 
     void Update()
     {
-        leftTime.sizeDelta = new Vector2(leftTime.sizeDelta.x, thisRect.rect.height * (game.timeLeft / 60f));
-        rightTime.sizeDelta = new Vector2(rightTime.sizeDelta.x, thisRect.rect.height * (game.timeLeft / 60f));
+        leftTime.sizeDelta = new Vector2(leftTime.sizeDelta.x, parentTimeRect.rect.height * (game.timeLeft / 60f));
+        rightTime.sizeDelta = new Vector2(rightTime.sizeDelta.x, parentTimeRect.rect.height * (game.timeLeft / 60f));
+
+        barrelPoints.text = game.player.points.ToString();
+        cubePoints.text = 0.ToString();
     }
 }
